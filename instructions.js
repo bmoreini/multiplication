d0c0299acc19bfd3065bc052153e695a56f0ab5a
bmoreini <42300498+bmoreini@users.noreply.github.com>
2020-01-08 09:29:15 -0500

    Update instructions.js

--- instructions.js
+++ instructions.js
@@ -10,16 +10,16 @@
 /* For Loop */
 // 4. Embed all code in a For loop and run it 3 times. End loop if someone entered "stop".
 
-/* multiPractice Function */
-// 5. Move code parts 1-3 into a function called multiPractice and call multiPractice within the For loop.
+/* questioner Function */
+// 5. Move code parts 1-3 into a function called questioner and call questioner within the For loop.
+// 6. Count how many wrong answers are given (up to 1 per question).
 
-/* multiPractice Error Count */
-// 6. Count how many not-right-first-answers and return that count from multiPractice.
+/* userInput Function */
+// 7. Make a separate function to process user input. Return wrong (0 or 1) to questioner.
 
 /* quiz results */
-// 7. At "stop" or loop end, report # answers wrong.  If no wrong answers, say "Charlie, you've won!"
+// 8. At "stop" or loop end, report # answers wrong.  If no wrong answers, say "Charlie, you've won!"
 
-/* setup */
-// 8. before beginning loop, ask user how many questions
-// 9. ask user what the top range for factors should be (e.g. 3-6)
+/* setup Function*/
+// 9. before beginning loop, ask user how many questions and ask user what the top range for factors should be (e.g. 3-6)
 // 10. ask user what the bottom range for factors should be (e.g. 2-5)
