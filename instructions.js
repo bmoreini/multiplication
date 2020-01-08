9650bf647db1fa1cb34995d9ac9326899b996ddb
bmoreini <42300498+bmoreini@users.noreply.github.com>
2020-01-08 09:21:53 -0500

    Update instructions.js

--- instructions.js
+++ instructions.js
@@ -1,16 +1,25 @@
-
 /* MULTIPLICATION QUIZ KATA */
 
-/* INSTRUCTIONS for PART 1 */
-
+/* Simple */
 // 1. Print 2 Random Integers, X & Y, each between 3 and 9, as a soluble equation:  X * Y = ??
 // 2. Receive manual input for solution, say "Correct!" if match, "Incorrect!" if not.
-// 3. Embed equation in While loop that continues until entry matches solution, or = "stop".
-// 4. Embed all code in a For loop and run it 10 times.
 
-/* INSTRUCTIONS for PART 2: REFACTOR INTO FUNCTIONS */
+/* While Loop */
+// 3. Embed input in While loop that continues until entry matches solution, or = "stop".
 
-// 5. Move code parts 1-3 into a function called multiPractice, and call multiPractice within the For loop.
+/* For Loop */
+// 4. Embed all code in a For loop and run it 3 times. End loop if someone entered "stop".
+
+/* multiPractice Function */
+// 5. Move code parts 1-3 into a function called multiPractice and call multiPractice within the For loop.
+
+/* multiPractice Error Count */
 // 6. Count how many not-right-first-answers and return that count from multiPractice.
+
+/* quiz results */
 // 7. At "stop" or loop end, report # answers wrong.  If no wrong answers, say "Charlie, you've won!"
-// 8. Extra Credit: instead of 10 times, run loop to value of questions variable entered manually. 
+
+/* setup */
+// 8. before beginning loop, ask user how many questions
+// 9. ask user what the top range for factors should be (e.g. 3-6)
+// 10. ask user what the bottom range for factors should be (e.g. 2-5)
